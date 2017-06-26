fb577dd9ef43b0f0ea6683751bb78c09f2a03362
Glenn Gartner <glenngartner@gmail.com>
2017-06-25 21:49:36 -0400

    basic bootstrap integrated, nav component generated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { RendererComponent } from './renderer/renderer.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RendererComponent
+    RendererComponent,
+    NavComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
