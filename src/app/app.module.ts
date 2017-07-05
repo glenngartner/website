34b801c860c055ffed31567bd338e8a929241cd5
Glenn Gartner <glenngartner@gmail.com>
2017-07-05 00:26:04 -0400

    complete simple router and nav links to router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { RendererComponent } from './renderer/renderer.component';
 import { NavComponent } from './nav/nav.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+  {path: 'home', component: HomeComponent, data: {title: 'Home'}},
+  {path: 'game', component: RendererComponent, data: {title: '3D App'}},
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: '**', component: PageNotFoundComponent}
+]
 
 @NgModule({
   declarations: [
@@ -14,9 +23,14 @@ import { HomeComponent } from './home/home.component';
     RendererComponent,
     NavComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    PageNotFoundComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      {enableTracing: true}
+    ),
     BrowserModule,
     NgbModule.forRoot()
   ],
